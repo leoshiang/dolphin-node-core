cabcf6703ebb20f88312ebdaa0bd3b142f1330d5
向皓田 <leoshiang@gmail.com>
2022-03-07 14:19:39 +0800

    0.0.4

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 const { Matrix } = require('./src/Matrix')
 const { DynamicMatrix } = require('./src/DynamicMatrix')
+const { TextFile } = require('./src/TextFile')
 const Type = require('./src/Type')
 
 module.exports = {
   DynamicMatrix,
   Matrix,
+  TextFile,
   Type,
 }
